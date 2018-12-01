84fabc97ad29bad576dd2702a3ff4db133fe72f0
Kevin Gnadinger <kevin@dialexa.com>
2018-12-01 07:40:52 -0600

    problem 2, day 1

--- Advent_day-1.playground/Contents.swift
+++ Advent_day-1.playground/Contents.swift
@@ -3,6 +3,8 @@ import Foundation
 let fileURL = Bundle.main.url(forResource: "advent_day_1_input", withExtension: "txt")
 let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
 
+
+// Problem 1
 let splitInput = content.split(separator: "\n")
 
 func convertToInt (object: String.SubSequence) -> Int {
@@ -11,5 +13,30 @@ func convertToInt (object: String.SubSequence) -> Int {
 
 let answer = splitInput.map(convertToInt).reduce(0, +)
 
-print(answer)
+// Problem 2
 
+func findFirstFrequencyRepeat(inputs: [Int], frequencyDict: Dictionary<Int, Int>, currentTotal: Int) -> Int {
+  var frequencies = frequencyDict
+  var runningTotal = currentTotal
+  for input in inputs {
+    runningTotal += input
+    if let currentFreq = frequencies[runningTotal] {
+      frequencies[runningTotal] = currentFreq + 1
+    } else {
+      frequencies[runningTotal] = 1
+    }
+    if frequencies[runningTotal] == 2 {
+      return runningTotal
+    }
+  }
+  return findFirstFrequencyRepeat(inputs: inputs, frequencyDict: frequencies, currentTotal: runningTotal)
+}
+
+let inputs = splitInput.map(convertToInt)
+var frequencies: Dictionary<Int, Int> = [:]
+frequencies[0] = 1
+var runningTotal = 0
+
+let solution = findFirstFrequencyRepeat(inputs: inputs, frequencyDict: frequencies, currentTotal: runningTotal)
+
+print(solution)
