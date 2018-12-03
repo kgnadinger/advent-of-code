c9b40d4ebf4cfc114c9302950eed27171a805bdf
Kevin Gnadinger <kevin@dialexa.com>
2018-12-03 11:07:01 -0600

    Day 3 Performance Explination

--- Day-3.playground/Contents.swift
+++ Day-3.playground/Contents.swift
@@ -10,7 +10,12 @@ let main = Main(fabric: fabric, input: splitInput)
 let result = main.run()
 print(result)
 
-
+/*
+ I found out that playground has some performance limitations today. Any large computation's will slow to a crawl if dont in the main playground file.
+ 
+ Instead, as I have done here, I've organized the computation into source files which are compiled and ran outside the playground environement. 
+ 
+*/
 
 
 
